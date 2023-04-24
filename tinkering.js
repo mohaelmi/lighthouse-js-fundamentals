c0ba8b0f4db641a239e16f3bc0f9bb9b48f8f05a
mohaelmi <moha.abdi1771@gmail.com>
2023-04-23 19:03:56 -0500

    added sayhello func

--- tinkering.js
+++ tinkering.js
@@ -1,7 +1,54 @@
-function multiply(a, b) {
-  return a * b;
+// function multiply(a, b) {
+//   return a * b;
+// }
+
+// const result = multiply(2, 4);
+
+// console.log(result);
+
+// const whichSchool  = function (age) {
+//   if(age < 13) {
+//     console.log("Elementary School")
+//   }else if(age <= 13 && age >= 18){
+//     console.log("Secondary School")
+//   }else {
+//     console.log("Lighthouse Labs")
+//   }
+// }
+
+// whichSchool(11)
+
+
+// function printNum() {
+//   let num = 100;
+//   while(num <= 200){
+//     if(num % 3 == 0){
+//       console.log(num)
+//     }else if(num % 4 == 0){
+//       console.log("Lighthouse")
+//     }else if(num % 3 == 0 && num % 4 == 0){
+//       console.log("LoopyLighthouse")
+//     }
+//     num++;
+//   }
+// }
+
+// printNum();
+
+
+
+let num = 100;
+while(num <= 200){
+ 
+  if(num % 3 === 0){
+    console.log("Loopy")
+  }
+  if(num % 4 === 0){
+    console.log("Lighthouse")
+  }
+  if(num % 12 === 0){
+    console.log(num)
+  }
+  num++;
+  //console.log(num)
 }
-
-const result = multiply(2, 4);
-
-console.log(result);
